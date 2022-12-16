d1924b68faa6dc44e52dfead01c69f6681aa479f
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2022-12-15 19:07:33 -0800

    Update Xeris_v1_PRIVATE.lua

--- Xeris_v1_PRIVATE.lua
+++ Xeris_v1_PRIVATE.lua
@@ -53,47 +53,6 @@ GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Version.Text = "Thanks for usin
 GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Position = UDim2.new(1, -175 - 20, 1, -25)
 infonotify("DO NOT LEAK", "Do not leak this Config", 5)
 
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until shared.GuiLibrary
-local uis = game:GetService("UserInputService")
-local GuiLibrary = shared.GuiLibrary
-local ScriptSettings = {}
-local UIS = game:GetService("UserInputService")
-local COB = function(tab, argstable) 
-	return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
-end
-function securefunc(func)
-	task.spawn(function()
-		spawn(function()
-			pcall(function()
-				loadstring(
-					func()
-				)()
-			end)
-		end)
-	end)
-end
-function warnnotify(title, content, duration)
-	local frame = GuiLibrary["CreateNotification"](title or "Windows", content or "(No Content Given)", duration or 5, "assets/WarningNotification.png")
-	frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 64, 64)
-end
-function infonotify(title, content, duration)
-	local frame = GuiLibrary["CreateNotification"](title or "Windows", content or "(No Content Given)", duration or 5, "assets/InfoNotification.png")
-	frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 64, 64)
-end
-function getstate()
-	local ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore
-	return ClientStoreHandler:getState().Game.matchState
-end
-function iscustommatch()
-	local ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore
-	return ClientStoreHandler:getState().Game.customMatch
-end
-function checklagback()
-	local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
-	return isnetworkowner(hrp)
-end
-
 local CustomFly = COB("Blatant", {
 	["Name"] = "Xeris Autowin",
 	["Function"] = function(callback)
