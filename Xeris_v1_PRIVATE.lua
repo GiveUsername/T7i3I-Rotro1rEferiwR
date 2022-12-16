20d6f607149f0709a1cba7a872f4a5842bfcb636
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2022-12-15 19:04:51 -0800

    Update Xeris_v1_PRIVATE.lua

--- Xeris_v1_PRIVATE.lua
+++ Xeris_v1_PRIVATE.lua
@@ -52,3 +52,126 @@ GuiLibrary["MainGui"].ScaledGui.ClickGui.MainWindow.TextLabel.Text = "Private Va
 GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Version.Text = "Thanks for using Xeris!"
 GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Position = UDim2.new(1, -175 - 20, 1, -25)
 infonotify("DO NOT LEAK", "Do not leak this Config", 5)
+
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until shared.GuiLibrary
+local uis = game:GetService("UserInputService")
+local GuiLibrary = shared.GuiLibrary
+local ScriptSettings = {}
+local UIS = game:GetService("UserInputService")
+local COB = function(tab, argstable) 
+	return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
+end
+function securefunc(func)
+	task.spawn(function()
+		spawn(function()
+			pcall(function()
+				loadstring(
+					func()
+				)()
+			end)
+		end)
+	end)
+end
+function warnnotify(title, content, duration)
+	local frame = GuiLibrary["CreateNotification"](title or "Windows", content or "(No Content Given)", duration or 5, "assets/WarningNotification.png")
+	frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 64, 64)
+end
+function infonotify(title, content, duration)
+	local frame = GuiLibrary["CreateNotification"](title or "Windows", content or "(No Content Given)", duration or 5, "assets/InfoNotification.png")
+	frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 64, 64)
+end
+function getstate()
+	local ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore
+	return ClientStoreHandler:getState().Game.matchState
+end
+function iscustommatch()
+	local ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore
+	return ClientStoreHandler:getState().Game.customMatch
+end
+function checklagback()
+	local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+	return isnetworkowner(hrp)
+end
+
+local CustomFly = COB("Blatant", {
+	["Name"] = "Xeris Autowin",
+	["Function"] = function(callback)
+		if callback then
+			pcall(function()
+				ScriptSettings.CustomFly = true
+				while task.wait() do
+					if not ScriptSettings.CustomFly == true then return end
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/T7i3I-Rotro1rEferiwR/main/AW.lua", true))()
+				end
+			end)
+		else
+			pcall(function()
+				ScriptSettings.CustomFly = false
+				game:GetService("Workspace").Gravity = 196.2
+			end)
+		end
+	end,
+	["Default"] = false,
+	["HoverText"] = "Automaticaly Wins the Game (Wait 7 Seconds)"
+})
+
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["wood_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["wood_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["wood_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["wood_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- stone
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["stone_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["stone_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["stone_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["stone_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- iron
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["iron_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["iron_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["iron_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["iron_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- diamond
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["diamond_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["diamond_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["diamond_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["diamond_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
