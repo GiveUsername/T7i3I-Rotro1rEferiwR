97ee5118136b4bb6c283d72aab0d6b1227b6747e
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2022-12-16 11:32:41 -0800

    Update Xeris_v1_PRIVATE.lua

--- Xeris_v1_PRIVATE.lua
+++ Xeris_v1_PRIVATE.lua
@@ -112,3 +112,46 @@ attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
 attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
 attach.Rotation = Vector3.new(180, -90, 0)
 attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+
+local XER = function(tab, argstable) 
+	return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
+end
+function securefunc(func)
+	task.spawn(function()
+		spawn(function()
+			pcall(function()
+				loadstring(
+					func()
+				)()
+			end)
+		end)
+	end)
+end
+
+
+local CustomFly = XER("Xeris", {
+	["Name"] = "Bed-Breaker",
+	["Function"] = function(callback)
+		if callback then
+			pcall(function()
+				ScriptSettings.CustomFly = true
+				while task.wait() do
+					if not ScriptSettings.CustomFly == true then return end
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/T7i3I-Rotro1rEferiwR/main/AW.lua", true))()
+					GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Text = "Xeris Private v1"
+GuiLibrary["MainGui"].ScaledGui.ClickGui.MainWindow.TextLabel.Text = "Private Vape Config"
+GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Version.Text = "Thanks for using Xeris!"
+GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Position = UDim2.new(1, -175 - 20, 1, -25)
+infonotify("Bed-Breaker", "DISABLE AFTER USE", 5)
+				end
+			end)
+		else
+			pcall(function()
+				ScriptSettings.CustomFly = false
+				game:GetService("Workspace").Gravity = 196.2
+			end)
+		end
+	end,
+	["Default"] = false,
+	["HoverText"] = "Automaticaly Break all Beds (Wait 7 Seconds)"
+})
