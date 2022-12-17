d6ed97c595a9fecc91c6f260d7874d8fc46546e3
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2022-12-16 16:25:24 -0800

    Update Xeris_v1_PRIVATE.lua

--- Xeris_v1_PRIVATE.lua
+++ Xeris_v1_PRIVATE.lua
@@ -53,7 +53,7 @@ GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Version.Text = "Thanks for usin
 GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Position = UDim2.new(1, -175 - 20, 1, -25)
 infonotify("DO NOT LEAK", "Do not leak this Config", 5)
 
-game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+game:GetObjects("rbxassetid://5410728622")[1].Parent = workspace
 
 workspace.Sword.Name = "Handle"
 workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
@@ -68,7 +68,7 @@ attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
 attach.Rotation = Vector3.new(180, -90, 0)
 attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
 -- stone
-game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+game:GetObjects("rbxassetid://5410728622")[1].Parent = workspace
 
 workspace.Sword.Name = "Handle"
 workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
@@ -83,7 +83,7 @@ attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
 attach.Rotation = Vector3.new(180, -90, 0)
 attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
 -- iron
-game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+game:GetObjects("rbxassetid://5410728622")[1].Parent = workspace
 
 workspace.Sword.Name = "Handle"
 workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
@@ -98,7 +98,7 @@ attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
 attach.Rotation = Vector3.new(180, -90, 0)
 attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
 -- diamond
-game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+game:GetObjects("rbxassetid://5410728622")[1].Parent = workspace
 
 workspace.Sword.Name = "Handle"
 workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
